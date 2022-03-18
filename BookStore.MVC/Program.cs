6edddcc36699eb4c60396e45f9959a9def58b995
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2022-03-18 02:55:10 +0200

    Hello, World!

--- BookStore.MVC/Program.cs
+++ BookStore.MVC/Program.cs
@@ -1,2 +1,26 @@
 ﻿
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello, World!");
+    });
+});
+
+app.Run();
