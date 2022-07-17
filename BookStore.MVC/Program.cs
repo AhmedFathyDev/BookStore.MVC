b44ad85b4e5f1beff3c09d34ee4f03e0683df0ec
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-07-17 23:58:02 +0200

    Update BookStore.MVC application

--- BookStore.MVC/Program.cs
+++ BookStore.MVC/Program.cs
@@ -1,23 +1,17 @@
-﻿
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+﻿var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
-app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapGet("/", async context =>
+    endpoints.Map("/", async (context) =>
     {
         await context.Response.WriteAsync("Hello, World!");
     });
